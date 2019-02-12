5dc26db506568346ee8e091d606d113d300131fc
Mikkel <mikkeleb@gmail.com>
2019-02-12 20:13:33 +0100

    Ting funker, men ikke tester. hub hub

---
+++ src/main/java/no/hvl/terningspillet/Tests/TerningspilletTests.java
@@ -0,0 +1,27 @@
+package no.hvl.terningspillet.Tests;
+
+import no.hvl.terningspillet.spillet.Spiller;
+import no.hvl.terningspillet.spillet.Terningspillet;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TerningspilletTests {
+    private Terningspillet spillet;
+
+    @BeforeAll
+    public void setup(){
+        spillet = new Terningspillet(1, 3);
+    }
+
+    @Test
+    public void testUavgjort(){
+        for (Spiller s : spillet.getSpillere()){
+            s.setVerdi(10);
+        }
+        assertTrue(spillet.isUavgjort());
+        assertEquals(3, spillet.hentVinnere().size());
+    }
+}
